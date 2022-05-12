57089ac045b8b6403a668519e65d67e1961cb9fe
GandyA23 <avila.gandy@gmail.com>
2022-05-12 11:44:56 -0500

    Se agrega la práctica 1 (Aplicación en consola)

---
+++ Practica01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Practica01 {
+    class Program {
+        const double DESCUENTO = 0.1;
+        const double PRECIO = 15.9;
+        const int CANTIDAD = 5;
+        static void Main (string[] args) {
+            int cantidadProducto = 12;
+            double total = 0.0;
+            int noPluma = cantidadProducto / CANTIDAD;
+
+            total += noPluma * CANTIDAD * PRECIO;
+
+            double descuentoTotal = total * DESCUENTO;
+            total += (cantidadProducto - (noPluma * CANTIDAD)) * PRECIO;
+
+            Console.WriteLine($"Se han regalado {noPluma} plumas");
+            Console.WriteLine($"Total: ${total} - Descuento (10% por cada 5 productos): ${descuentoTotal} - Total Neto: ${(total-descuentoTotal)}");
+        } 
+    }
+}
---
+++ Practica01/README.md
@@ -0,0 +1,11 @@
+# Problema 1
+
+Una tienda vende todo a un precio  $15.90, y esta tiene la siguiente promoción:
+
+Por cada 5 productos, se hace un descuento de 10% de descuento a ese grupo en específico y se regala una pluma de regalo. 
+
+Crea una aplicación en consola que dado la cantidad de productos me diga lo siguiente:
+ - La cantidad de plumas que se regalan
+ - El total sin descuento
+ - La cantidad que se va a descontar
+ - El total neto
