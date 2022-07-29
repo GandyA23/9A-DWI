06fced447b11f21858fffaa58fc618c65ec3a0be
GandyA23 <avila.gandy@gmail.com>
2022-07-29 15:24:25 -0500

    Práctica 7 finalizada

--- ExperienciaApp/Controllers/ContactoController.cs
+++ ExperienciaApp/Controllers/ContactoController.cs
@@ -38,6 +38,52 @@ namespace ExperienciaApp.Controllers
             return RedirectToAction("Index");
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Actualizar(int id)
+        {
+                var contacto = await _contactoRepository.BuscarPorId(id);
+                if(contacto is null)
+                {
+                    return RedirectToAction("Index");
+                }
+                return View(contacto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Actualizar(Contacto contacto)
+        {
+                var existeContacto = await _contactoRepository.BuscarPorId(contacto.Id);
+                if(existeContacto is null)
+                {
+                    return RedirectToAction("Index");
+                }
+                await _contactoRepository.Actualizar(contacto);
+                return RedirectToAction("Index");
+        } 
+
+        [HttpGet]
+        public async Task<IActionResult> Eliminar(int id)
+        {
+                var existeContacto = await _contactoRepository.BuscarPorId(id);
+                if(existeContacto is null)
+                {
+                    return RedirectToAction("Index");
+                }
+                await _contactoRepository.Eliminar(id);
+                return RedirectToAction("Listar");
+        }
+
+        [AcceptVerbs("GET", "POST")]
+        public async Task<IActionResult> VerificarExisteCorreo(string correoElectronico)
+        {
+                var existeCorreo = await _contactoRepository.VerificarExisteCorreo(correoElectronico);
+                if(existeCorreo)
+                {
+                    return Json($"El correo {correoElectronico} ya ha sido registrado.");
+                }
+                return Json(true);
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
